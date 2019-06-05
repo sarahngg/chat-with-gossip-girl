705f0de3f685fbdb5218883515ece25b738cc967
Sarah Ng <sarah.ng26@gmail.com>
2019-06-05 10:37:13 -0700

    Updates

---
+++ precache-manifest.b884737880903959461cc6053292d87c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd84665625591a3d9270ac01bbeb657",
+    "url": "/chat-with-gossip-girl/index.html"
+  },
+  {
+    "revision": "dfad77a66c7ee35a49a2",
+    "url": "/chat-with-gossip-girl/static/css/main.c11ac755.chunk.css"
+  },
+  {
+    "revision": "17d33c3b63546c6d1762",
+    "url": "/chat-with-gossip-girl/static/js/2.eefe7e96.chunk.js"
+  },
+  {
+    "revision": "dfad77a66c7ee35a49a2",
+    "url": "/chat-with-gossip-girl/static/js/main.d40505a7.chunk.js"
+  },
+  {
+    "revision": "afc0dc41bf5a3ce91bd5",
+    "url": "/chat-with-gossip-girl/static/js/runtime~main.db4396cc.js"
+  }
+]);
