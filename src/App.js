b6e2795e595c50edfe227967123f9e9787afcfb8
Sarah Ng <sarah.ng26@gmail.com>
2019-06-12 22:57:07 -0700

    Added CORS to API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import SendTips from './components/SendTips';
 // import { connect } from 'react-redux';
 
 class App extends Component {
+
+    constructor(props) { // INITIALIZES THE DEFAULT STATE
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    callAPI() { // FETCH DATA FROM API AND STORE RESPONSE ON this.state.apiResponse
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+    
+    componentDidMount() {
+        this.callAPI();
+    }
+    
+
     render() {
         //console.log(this.props);
         const { tips } = this.props;
@@ -18,10 +36,12 @@ class App extends Component {
                 </div>
                 <TipLine tips={tips}/>
                 <SendTips />
+                <p className="App-intro">{this.state.apiResponse}</p>
             </div>
         )
     }
 }
+
 
 export default App;
 
